162484881e7d2259ae2efe0ab6704eccae303aba
Arkady Seleznev <a.seleznev@me.com>
2021-11-07 21:01:20 +0700

    chore: accompanying changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,28 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { config } from "./config/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { typeOrmConfig } from "./config/typeorm.config";
+import { PostgresModule } from "./postgres/postgres.module";
+import { MongoModule } from "./mongo/mongo.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { GraphQLModule } from "@nestjs/graphql";
+import { join } from "path";
 
 @Module({
-  imports: [],
+  imports: [
+    PostgresModule,
+    MongoModule,
+    ConfigModule.forRoot(config),
+    TypeOrmModule.forRootAsync(typeOrmConfig),
+    MongooseModule.forRoot('mongodb://localhost/database-benchmark-mongo'),
+    GraphQLModule.forRoot({
+      installSubscriptionHandlers: true,
+      autoSchemaFile: join(process.cwd(), 'schema.gql'),
+      playground: true,
+      introspection: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from "@nestjs/config";
+
+const DEFAULT_APP_HORT = 'localhost'
+const DEFAULT_APP_PORT = 3000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService)
+
+  const port = configService.get('PORT') || DEFAULT_APP_PORT
+  const hostname = configService.get('HOST') || DEFAULT_APP_HORT
+
+  await app.listen(port, hostname, () => console.log(`Server running at ${hostname}:${port}`))
+
 }
 bootstrap();
